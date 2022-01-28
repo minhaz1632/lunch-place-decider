0c9448d38b1e3763220ef0f3891d9808001f4ecb
minhaz1632 <minhaz1632@gmail.com>
2022-01-28 23:24:13 +0600

    Add basic api documentation schema

--- lunch_place_decider/urls.py
+++ lunch_place_decider/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
+    path('api-docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
+    path('openapi/', get_schema_view(
+            title="Lunch place decider",
+            description="Vote for lunch place",
+            version="1.0.0"
+        ), name='openapi-schema'),
 ]
